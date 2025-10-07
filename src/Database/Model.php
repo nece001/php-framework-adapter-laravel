1106cb4d5ed5af4a71f57485c56baa598c63c9fc
nece001 <nece001@163.com>
2025-10-07 15:14:30 +0800

    修改表名

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -2,12 +2,12 @@
 
 namespace Nece\Framework\Adapter\Database;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model as EloquentModel;
 use Nece\Framework\Adapter\Contract\DataBase\IModel;
 
 class Model extends EloquentModel implements IModel
 {
-
     const DELETED_AT = 'deleted_time';
 
     // 指定使用的查询类
@@ -62,9 +62,32 @@ class Model extends EloquentModel implements IModel
      */
     public function getTableName(): string
     {
-        return $this->table;
+        return $this->getTable();
     }
 
+    /**
+     * 表名
+     * 重写父类方法，解决表名复数问题
+     *
+     * @author nece001@163.com
+     * @create 2025-09-13 22:35:32
+     *
+     * @return string
+     */
+    public function getTable()
+    {
+        return $this->table ?? Str::snake(class_basename($this));
+    }
+
+    /**
+     * 别名
+     *
+     * @author nece001@163.com
+     * @create 2025-10-07 15:13:40
+     *
+     * @param string $alias
+     * @return Query
+     */
     public static function alias(string $alias)
     {
         $model = new static();
