d00030dc29c088df8ce703b9adfd7fcc1e4db52d
nece001 <nece001@163.com>
2025-10-09 21:31:37 +0800

    修改查询

--- src/Database/Db.php
+++ src/Database/Db.php
@@ -2,10 +2,10 @@
 
 namespace Nece\Framework\Adapter\Database;
 
-use Closure;
 use Illuminate\Support\Facades\DB as FacadesDB;
 use Nece\Framework\Adapter\Contract\DataBase\IDbManater;
 use Illuminate\Database\Query\Builder;
+use Nece\Framework\Adapter\Contract\DataBase\IQuery;
 
 /**
  * @see \Illuminate\Database\DatabaseManager
@@ -20,12 +20,12 @@ class Db implements IDbManater
      * @create 2025-10-05 11:28:24
      *
      * @param string $table 表名
-     * @param string $as 别名
+     * @param string $alias 别名
      * @return Builder
      */
-    public static function table($table, $as = null)
+    public static function table($table, $alias = ''): IQuery
     {
-        return FacadesDB::table($table, $as);
+        return new Query($table, $alias);
     }
 
     /**
@@ -131,221 +131,5 @@ class Db implements IDbManater
     public static function rawMax(string $field, string $alias)
     {
         return self::rawFunc('MAX', $field, $alias);
-    }
-
-    /**
-     * 查询单条记录
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @param  bool  $useReadPdo
-     * @return mixed
-     */
-    public static function selectOne($query, $bindings = [], $useReadPdo = true)
-    {
-        return FacadesDB::selectOne($query, $bindings, $useReadPdo);
-    }
-
-    /**
-     * 查询单个字段值
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @param  bool  $useReadPdo
-     * @return mixed
-     *
-     * @throws RuntimeException
-     */
-    public static function scalar($query, $bindings = [], $useReadPdo = true)
-    {
-        return FacadesDB::scalar($query, $bindings, $useReadPdo);
-    }
-
-    /**
-     * 查询多条记录
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @param  bool  $useReadPdo
-     * @return array
-     */
-    public static function select($query, $bindings = [], $useReadPdo = true)
-    {
-        return FacadesDB::select($query, $bindings, $useReadPdo);
-    }
-
-    /**
-     * 查询多条记录并返回生成器
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @param  bool  $useReadPdo
-     * @return \Generator
-     */
-    public static function cursor($query, $bindings = [], $useReadPdo = true)
-    {
-        return FacadesDB::cursor($query, $bindings, $useReadPdo);
-    }
-
-    /**
-     * 执行插入SQL语句
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @return bool
-     */
-    public static function insert($query, $bindings = [])
-    {
-        return FacadesDB::insert($query, $bindings);
-    }
-
-    /**
-     * 执行更新SQL语句
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @return int
-     */
-    public static function update($query, $bindings = [])
-    {
-        return FacadesDB::update($query, $bindings);
-    }
-
-    /**
-     * 执行删除SQL语句
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @return int
-     */
-    public static function delete($query, $bindings = [])
-    {
-        return FacadesDB::delete($query, $bindings);
-    }
-
-    /**
-     * 执行SQL语句并返回布尔结果
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @return bool
-     */
-    public static function statement($query, $bindings = [])
-    {
-        return FacadesDB::statement($query, $bindings);
-    }
-
-    /**
-     * 执行SQL语句并返回受影响的行数
-     *
-     * @param  string  $query
-     * @param  array  $bindings
-     * @return int
-     */
-    public static function affectingStatement($query, $bindings = [])
-    {
-        return FacadesDB::affectingStatement($query, $bindings);
-    }
-
-    /**
-     * 执行原始SQL语句
-     *
-     * @param  string  $query
-     * @return bool
-     */
-    public static function unprepared($query)
-    {
-        return FacadesDB::unprepared($query);
-    }
-
-    /**
-     * 准备SQL语句绑定参数
-     *
-     * @param  array  $bindings
-     * @return array
-     */
-    public static function prepareBindings(array $bindings)
-    {
-        return FacadesDB::prepareBindings($bindings);
-    }
-
-    /**
-     * 执行数据库事务
-     *
-     * @param  \Closure  $callback
-     * @param  int  $attempts
-     * @return mixed
-     *
-     * @throws \Throwable
-     */
-    public static function transaction(Closure $callback, $attempts = 1)
-    {
-        return FacadesDB::transaction($callback, $attempts);
-    }
-
-    /**
-     * 开始数据库事务
-     *
-     * @return void
-     */
-    public static function beginTransaction()
-    {
-        FacadesDB::beginTransaction();
-    }
-
-    /**
-     * 提交当前数据库事务
-     *
-     * @return void
-     */
-    public static function commit()
-    {
-        FacadesDB::commit();
-    }
-
-    /**
-     * 回滚当前数据库事务
-     *
-     * @return void
-     */
-    public static function rollBack()
-    {
-        FacadesDB::rollBack();
-    }
-
-    /**
-     * 获取当前数据库事务层级
-     *
-     * @return int
-     */
-    public static function transactionLevel()
-    {
-        return FacadesDB::transactionLevel();
-    }
-
-    /**
-     * 执行给定回调函数在"dry run"模式下
-     *
-     * @param  \Closure  $callback
-     * @return array
-     */
-    public static function pretend(Closure $callback)
-    {
-        return FacadesDB::pretend($callback);
-    }
-
-    /**
-     * 获取当前数据库连接的数据库名
-     *
-     * @return string
-     */
-    public static function getDatabaseName()
-    {
-        return FacadesDB::getDatabaseName();
-    }
-
-    public static function __callStatic($name, $arguments)
-    {
-        return FacadesDB::$name(...$arguments);
     }
 }
--- src/Database/Query.php
+++ src/Database/Query.php
@@ -2,11 +2,12 @@
 
 namespace Nece\Framework\Adapter\Database;
 
+use Closure;
+use Illuminate\Support\Facades\DB;
 use Nece\Framework\Adapter\Contract\DataBase\IQuery;
 use Nece\Gears\Dto;
 use Nece\Gears\PagingCollection;
 use Nece\Gears\PagingVar;
-use Illuminate\Database\Eloquent\Builder;
 
 /**
  * 查询类
@@ -14,90 +15,516 @@ use Illuminate\Database\Eloquent\Builder;
  * @author nece001@163.com
  * @create 2025-10-08 10:09:19
  * 
- * @see Illuminate\Database\Eloquent\Builder
+ * @see Illuminate\Database\Query\Builder
  */
-class Query extends Builder implements IQuery
+class Query implements IQuery
 {
+    /**
+     * 数据库查询对象
+     *
+     * @var \Illuminate\Database\Query\Builder
+     */
+    private $query;
+
+    /**
+     * 表别名
+     *
+     * @var string
+     */
     private $alias = '';
 
-    public function getTable()
+    public function __construct(string $table, string $alias = '')
     {
-        return $this->model->getTable();
+        $this->query = DB::table($table, $alias);
+        if ($alias) {
+            $this->alias = $alias;
+        }
     }
 
-    public function getAlias()
+    /**
+     * @inheritDoc
+     */
+    public function getAlias(): string
     {
         return $this->alias;
     }
 
-    public function field($columns = ['*']): self
+    /**
+     * @inheritDoc
+     */
+    public function distinct(bool $distinct = true): self
     {
-        $this->query->select($columns);
-        return $this;
-    }
-
-    public function alias(string $alias): self
-    {
-        $this->alias = $alias;
-        $this->query->from($this->model->getTable(), $alias);
-        return $this;
-    }
-
-    public function order($column, $direction = 'asc')
-    {
-        $this->query->orderBy($column, $direction);
-        return $this;
-    }
-
-    public function group(...$groups)
-    {
-        $this->query->groupBy(...$groups);
+        $this->query->distinct($distinct);
         return $this;
     }
 
     /**
-     * 按分页查询
-     *
-     * @author nece001@163.com
-     * @create 2025-09-14 13:27:17
-     *
-     * @return PagingCollection
+     * @inheritDoc
      */
-    public function paging(PagingVar $paging): PagingCollection
+    public function field(string|array $field): self
     {
-        $page = $paging->getPage();
-        $size = $paging->getPageSize();
-        $list = new PagingCollection([], 0, $page, $size, $paging->getPageVarName(), $paging->getPageSizeVarName());
-        $items = $this->paginate($size, ['*'], $paging->getPageVarName(), $page);
-        if ($items) {
-            $list->setTotal($items->total());
-            $list->setCurrentPage($items->currentPage());
+        $this->query->select($field);
+        return $this;
+    }
 
-            foreach ($items as $item) {
-                $list->add(new Dto($item->toArray()));
-            }
+    /**
+     * @inheritDoc
+     */
+    public function join(string|array $table, Closure $on, string $type = 'INNER'): self
+    {
+        if (is_array($table)) {
+            $key = array_key_first($table);
+            $table = $key . ' AS ' . $table[$key];
         }
 
-        return $list;
+        $this->query->join($table, $on, $type);
+        return $this;
     }
 
     /**
-     * 查询所有
-     *
-     * @author nece001@163.com
-     * @create 2025-10-07 10:24:54
-     *
-     * @return array
+     * @inheritDoc
+     */
+    public function leftJoin(string|array $table, Closure $on): self
+    {
+        $this->join($table, $on, 'LEFT');
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rightJoin(string|array $table, Closure $on): self
+    {
+        $this->join($table, $on, 'RIGHT');
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function fullJoin(string|array $table, Closure $on): self
+    {
+        $this->join($table, $on, 'FULL');
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function crossJoin(string|array $table, Closure $on): self
+    {
+        $this->join($table, $on, 'CROSS');
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function where($field, $op = null, $condition = null): self
+    {
+        $this->query->where($field, $op, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function order(string $field, string $direction = 'asc'): self
+    {
+        $this->query->orderBy($field, $direction);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function group(string $field): self
+    {
+        $this->query->groupBy($field);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function groupByRaw(string $group): self
+    {
+        $this->query->groupByRaw($group);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function having(string $field, string $operator, $value): self
+    {
+        $this->query->having($field, $operator, $value);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function page(int $page, int $limit): self
+    {
+        $this->query->forPage($page, $limit);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function limit(int $limit): self
+    {
+        $this->query->limit($limit);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function lock(bool $lock = true): self
+    {
+        $this->query->lock($lock);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function comment(string $comment): self
+    {
+        $this->query->comment($comment);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function union(Closure $closure): self
+    {
+        $this->query->union($closure);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function partition(string $partition): self
+    {
+        $this->query->partition($partition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function value(string $field, $default = null)
+    {
+        return $this->query->value($field, $default);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function first()
+    {
+        return $this->query->first();
+    }
+
+    /**
+     * @inheritDoc
      */
     public function fetch(): array
     {
         $list = array();
-        $items = $this->get();
+        $items = $this->query->select();
         if ($items) {
-            foreach ($items as $item) {
-                $list[] = new Dto($item->toArray());
+            foreach ($items as $row) {
+                $list[] = new Dto((array)$row);
             }
         }
         return $list;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function paginate(PagingVar $paging): PagingCollection
+    {
+        $page = $paging->getPage();
+        $size = $paging->getPageSize();
+
+        $list = new PagingCollection([], 0, $page, $size, $paging->getPageVarName(), $paging->getPageSizeVarName());
+        $items = $this->query->paginate($size, ['*'], $paging->getPageVarName(), $page);
+        if ($items) {
+            $list->setTotal($items->total());
+            $list->setCurrentPage($items->currentPage());
+
+            foreach ($items as $row) {
+                $list->add(new Dto((array)$row));
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function chunk(int $size, callable $callback, ?string $column = null, string $order = 'asc')
+    {
+        return $this->query->chunk($size, $callback, $column, $order);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function toSql(): string
+    {
+        return $this->query->buildSql();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOr($field, $op = null, $condition = null): self
+    {
+        $this->query->orWhere($field, $op, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereXor($field, $op = null, $condition = null): self
+    {
+        $this->query->whereXor($field, $op, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereNull(string $field, string $logic = 'AND'): self
+    {
+        $this->query->whereNull($field, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereNotNull(string $field, string $logic = 'AND'): self
+    {
+        $this->query->whereNotNull($field, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrNull(string $field): self
+    {
+        $this->query->orWhereNull($field);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrNotNull(string $field): self
+    {
+        $this->query->orWhereNotNull($field);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereExists($condition, string $logic = 'AND'): self
+    {
+        $this->query->whereExists($condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereNotExists($condition, string $logic = 'AND'): self
+    {
+        $this->query->whereNotExists($condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereIn(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereIn($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrIn(string $field, $condition): self
+    {
+        $this->query->orWhereIn($field, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereNotIn(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereNotIn($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrNotIn(string $field, $condition): self
+    {
+        $this->query->orWhereNotIn($field, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereLike(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereLike($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrLike(string $field, $condition): self
+    {
+        $this->query->orWhereLike($field, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereNotLike(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereNotLike($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrNotLike(string $field, $condition): self
+    {
+        $this->query->orWhereNotLike($field, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereBetween(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereBetween($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereNotBetween(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereNotBetween($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrBetween(string $field, $condition): self
+    {
+        $this->query->orWhereBetween($field, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrNotBetween(string $field, $condition): self
+    {
+        $this->query->orWhereNotBetween($field, $condition);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereJsonContains(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->whereJsonContains($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrJsonContains(string $field, $condition, string $logic = 'AND'): self
+    {
+        $this->query->orWhereJsonContains($field, $condition, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereColumn(string $field1, string $operator, ?string $field2 = null, string $logic = 'AND'): self
+    {
+        $this->query->whereColumn($field1, $operator, $field2, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrColumn(string $field1, string $operator, ?string $field2 = null): self
+    {
+        $this->query->orWhereColumn($field1, $operator, $field2);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereRaw(string $where, array $bind = [], string $logic = 'AND'): self
+    {
+        $this->query->whereRaw($where, $bind, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function whereOrRaw(string $where, array $bind = [], string $logic = 'AND'): self
+    {
+        $this->query->orWhereRaw($where, $bind, $logic);
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function when($condition, Closure | array $query, Closure | array | null $otherwise = null): self
+    {
+        $this->query->when($condition, $query, $otherwise);
+        return $this;
     }
 }
