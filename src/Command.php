b14d8c2d8155a1c7d15eb0dabaa4aec0d1af8f62
nece001 <nece001@163.com>
2025-10-12 12:27:10 +0800

    添加兼容方法

--- src/Command.php
+++ src/Command.php
@@ -5,4 +5,15 @@ namespace Nece\Framework\Adapter;
 use Illuminate\Console\Command as ConsoleCommand;
 use Nece\Framework\Adapter\Contract\ICommand;
 
-abstract class Command extends ConsoleCommand implements ICommand {}
+abstract class Command extends ConsoleCommand implements ICommand
+{
+    protected function write(string $message)
+    {
+        return $this->output->write($message);
+    }
+
+    protected function writeln(string $message)
+    {
+        return $this->output->writeln($message);
+    }
+}
